9af1df1298b6e062b1ca58337fa625e7c09fb9df
yotamjacob <yotamjacob@gmail.com>
2025-05-12 13:01:44 +0300

    refactor analytics tests to uncomment view deletion hover action for improved clarity

--- cypress/tests/violet_app/analytics/analytics.cy.js
+++ cypress/tests/violet_app/analytics/analytics.cy.js
@@ -172,10 +172,11 @@ describe("analytics Test Suite", () => {
       cy.contains("Views", { timeout: 10000 }).click();
 
       //Delete the view
-      cy.contains(
-        "button.font-inter.flex.justify-between.group.items-center.relative.leading-3.px-2\\.5.font-medium.w-full.text-start.rounded-md.text-\\[13px\\].text-main-primaryDarkBlue.hover\\:bg-gray-150",
-        AUTOMATION_VIEW_NAME
-      ).realHover();
+      // cy.contains(
+      //   "button.font-inter.flex.justify-between.group.items-center.relative.leading-3.px-2\\.5.font-medium.w-full.text-start.rounded-md.text-\\[13px\\].text-main-primaryDarkBlue.hover\\:bg-gray-150",
+      //   AUTOMATION_VIEW_NAME
+      // ).realHover();
+      cy.contains(AUTOMATION_VIEW_NAME).realHover();
 
       cy.clickVisibleThreeDots();
       cy.clickOnDeleteViewAndVerify();
