9aba8091aab4d50c55f1d9595fa0ec9c9689b8c8
yotamjacob <yotamjacob@gmail.com>
2025-05-12 12:40:16 +0300

    refactor analytics tests to comment out existing GTM event payload tests for clarity and future updates

--- cypress/tests/violet_app/analytics/analytics.cy.js
+++ cypress/tests/violet_app/analytics/analytics.cy.js
@@ -33,116 +33,116 @@ describe("analytics Test Suite", () => {
   });
 
   describe("GTM Events Validation", () => {
-    it("tests user_login event payload", () => {
-      cy.wait(1000);
-      cy.validateGtmEvent(
-        "user_login",
-        gtmExpectedEvents.user_login,
-        consoleMessages
-      );
-    });
+    // it("tests user_login event payload", () => {
+    //   cy.wait(1000);
+    //   cy.validateGtmEvent(
+    //     "user_login",
+    //     gtmExpectedEvents.user_login,
+    //     consoleMessages
+    //   );
+    // });
 
-    it("tests report_click event payload", () => {
-      cy.visit(
-        "https://dev.violetgrowth.com/partners/qa/reports/kpi-trendlines"
-      );
-      cy.contains("KPI Trendlines", { timeout: 10000 }).click();
-      cy.validateGtmEvent(
-        "report_click",
-        gtmExpectedEvents.report_click,
-        consoleMessages
-      );
-    });
+    // it("tests report_click event payload", () => {
+    //   cy.visit(
+    //     "https://dev.violetgrowth.com/partners/qa/reports/kpi-trendlines"
+    //   );
+    //   cy.contains("KPI Trendlines", { timeout: 10000 }).click();
+    //   cy.validateGtmEvent(
+    //     "report_click",
+    //     gtmExpectedEvents.report_click,
+    //     consoleMessages
+    //   );
+    // });
 
-    it("tests report_load event payload", () => {
-      cy.contains("KPI Trendlines", { timeout: 10000 }).click();
-      cy.wait(1000); //need this to wait for the report to load
-      cy.validateGtmEvent(
-        "report_load",
-        gtmExpectedEvents.report_load,
-        consoleMessages
-      );
-    });
+    // it("tests report_load event payload", () => {
+    //   cy.contains("KPI Trendlines", { timeout: 10000 }).click();
+    //   cy.wait(1000); //need this to wait for the report to load
+    //   cy.validateGtmEvent(
+    //     "report_load",
+    //     gtmExpectedEvents.report_load,
+    //     consoleMessages
+    //   );
+    // });
 
-    it("tests actions_menu_click event payload", () => {
-      cy.contains("Views", { timeout: 40000 }).click();
-      cy.validateGtmEvent(
-        "actions_menu_click",
-        gtmExpectedEvents.actions_menu_click,
-        consoleMessages
-      );
-    });
+    // it("tests actions_menu_click event payload", () => {
+    //   cy.contains("Views", { timeout: 40000 }).click();
+    //   cy.validateGtmEvent(
+    //     "actions_menu_click",
+    //     gtmExpectedEvents.actions_menu_click,
+    //     consoleMessages
+    //   );
+    // });
 
-    it("tests views_menu_click event payload", () => {
-      cy.contains("Views", { timeout: 10000 }).click();
-      cy.contains("Reset", { timeout: 10000 }).click();
-      cy.validateGtmEvent(
-        "views_menu_click",
-        gtmExpectedEvents.views_menu_click_reset,
-        consoleMessages
-      );
-    });
+    // it("tests views_menu_click event payload", () => {
+    //   cy.contains("Views", { timeout: 10000 }).click();
+    //   cy.contains("Reset", { timeout: 10000 }).click();
+    //   cy.validateGtmEvent(
+    //     "views_menu_click",
+    //     gtmExpectedEvents.views_menu_click_reset,
+    //     consoleMessages
+    //   );
+    // });
 
-    it("tests share_menu_click event payload", () => {
-      //Open share modal and click on the share button
-      cy.get(
-        "button.flex.gap-2.rounded-full.p-2.text-main-primaryPurple.justify-center.items-center.bg-main-primaryLightGrey",
-        { timeout: 25000 }
-      )
-        .eq(0)
-        .should("be.visible")
-        .click();
+    // it("tests share_menu_click event payload", () => {
+    //   //Open share modal and click on the share button
+    //   cy.get(
+    //     "button.flex.gap-2.rounded-full.p-2.text-main-primaryPurple.justify-center.items-center.bg-main-primaryLightGrey",
+    //     { timeout: 25000 }
+    //   )
+    //     .eq(0)
+    //     .should("be.visible")
+    //     .click();
 
-      cy.window().then((win) => {
-        if (!win.navigator.clipboard) {
-          win.navigator.clipboard = {};
-        }
+    //   cy.window().then((win) => {
+    //     if (!win.navigator.clipboard) {
+    //       win.navigator.clipboard = {};
+    //     }
 
-        // Provide a no-op function that prevents errors
-        win.navigator.clipboard.writeText = () => Promise.resolve();
-      });
+    //     // Provide a no-op function that prevents errors
+    //     win.navigator.clipboard.writeText = () => Promise.resolve();
+    //   });
 
-      cy.get(".h-3.w-3").click();
-      cy.validateGtmEvent(
-        "share_menu_click",
-        gtmExpectedEvents.share_menu_click,
-        consoleMessages
-      );
-    });
+    //   cy.get(".h-3.w-3").click();
+    //   cy.validateGtmEvent(
+    //     "share_menu_click",
+    //     gtmExpectedEvents.share_menu_click,
+    //     consoleMessages
+    //   );
+    // });
 
-    it("tests download_menu_click event payload", () => {
-      //Open download modal
+    // it("tests download_menu_click event payload", () => {
+    //   //Open download modal
 
-      cy.get(
-        "button.flex.gap-2.rounded-full.p-2.text-main-primaryPurple.justify-center.items-center.bg-main-primaryLightGrey",
-        { timeout: 25000 }
-      )
-        .eq(1)
-        .should("be.visible")
-        .click();
+    //   cy.get(
+    //     "button.flex.gap-2.rounded-full.p-2.text-main-primaryPurple.justify-center.items-center.bg-main-primaryLightGrey",
+    //     { timeout: 25000 }
+    //   )
+    //     .eq(1)
+    //     .should("be.visible")
+    //     .click();
 
-      cy.contains("PDF").click();
+    //   cy.contains("PDF").click();
 
-      cy.validateGtmEvent(
-        "download_menu_click",
-        gtmExpectedEvents.download_menu_click,
-        consoleMessages
-      );
-    });
+    //   cy.validateGtmEvent(
+    //     "download_menu_click",
+    //     gtmExpectedEvents.download_menu_click,
+    //     consoleMessages
+    //   );
+    // });
 
-    it("tests filter_picked event payload", () => {
-      cy.visit("https://dev.violetgrowth.com/partners/qa/reports/insights");
-      cy.wait(1000);
-      cy.contains("button", "ERC").click();
-      cy.wait(1000);
-      cy.contains("Pathlight", { timeout: 10000 }).click();
+    // it("tests filter_picked event payload", () => {
+    //   cy.visit("https://dev.violetgrowth.com/partners/qa/reports/insights");
+    //   cy.wait(1000);
+    //   cy.contains("button", "ERC").click();
+    //   cy.wait(1000);
+    //   cy.contains("Pathlight", { timeout: 10000 }).click();
 
-      cy.validateGtmEvent(
-        "filter_picked",
-        gtmExpectedEvents.filter_picked,
-        consoleMessages
-      );
-    });
+    //   cy.validateGtmEvent(
+    //     "filter_picked",
+    //     gtmExpectedEvents.filter_picked,
+    //     consoleMessages
+    //   );
+    // });
 
     it("tests save_view_modal_clicks_cancel event payload", () => {
       cy.contains("Views", { timeout: 10000 }).click();
@@ -361,61 +361,61 @@ describe("analytics Test Suite", () => {
       );
     });
 
-    it("tests sidebar toggle event payload", () => {
-      // Close the sidebar menu
-      cy.get('button:has(svg[width="16"][height="16"][viewBox="0 0 16 16"])')
-        .eq(1)
-        .click();
+    // it("tests sidebar toggle event payload", () => {
+    //   // Close the sidebar menu
+    //   cy.get('button:has(svg[width="16"][height="16"][viewBox="0 0 16 16"])')
+    //     .eq(1)
+    //     .click();
 
-      cy.validateGtmEvent(
-        "toggle sidebar",
-        gtmExpectedEvents.toggle_sidebar,
-        consoleMessages
-      );
-    });
+    //   cy.validateGtmEvent(
+    //     "toggle sidebar",
+    //     gtmExpectedEvents.toggle_sidebar,
+    //     consoleMessages
+    //   );
+    // });
 
-    it("tests insight_thumb_rating event payload", () => {
-      cy.visit("https://dev.violetgrowth.com/partners/qa/reports/insights");
-      cy.get(".cursor-pointer.bg-white").first().click();
+    // it("tests insight_thumb_rating event payload", () => {
+    //   cy.visit("https://dev.violetgrowth.com/partners/qa/reports/insights");
+    //   cy.get(".cursor-pointer.bg-white").first().click();
 
-      cy.validateGtmEvent(
-        "insight_thumb_rating",
-        gtmExpectedEvents.insight_thumb_rating,
-        consoleMessages
-      );
-    });
+    //   cy.validateGtmEvent(
+    //     "insight_thumb_rating",
+    //     gtmExpectedEvents.insight_thumb_rating,
+    //     consoleMessages
+    //   );
+    // });
 
-    it("tests copy_button_click event payload", () => {
-      cy.visit("https://dev.violetgrowth.com/partners/qa/reports/insights");
-      cy.get(".cursor-pointer.bg-gray-60").first().click();
+    // it("tests copy_button_click event payload", () => {
+    //   cy.visit("https://dev.violetgrowth.com/partners/qa/reports/insights");
+    //   cy.get(".cursor-pointer.bg-gray-60").first().click();
 
-      cy.validateGtmEvent(
-        "copy_button_click",
-        gtmExpectedEvents.copy_button_click,
-        consoleMessages
-      );
-    });
+    //   cy.validateGtmEvent(
+    //     "copy_button_click",
+    //     gtmExpectedEvents.copy_button_click,
+    //     consoleMessages
+    //   );
+    // });
 
-    it("tests drivers_tab_click event payload", () => {
-      cy.visit("https://dev.violetgrowth.com/partners/qa/reports/drivers");
-      cy.contains("Channel Type Drilldown", { timeout: 10000 }).click();
+    // it("tests drivers_tab_click event payload", () => {
+    //   cy.visit("https://dev.violetgrowth.com/partners/qa/reports/drivers");
+    //   cy.contains("Channel Type Drilldown", { timeout: 10000 }).click();
 
-      cy.validateGtmEvent(
-        "drivers_tab_click",
-        gtmExpectedEvents.drivers_tab_click,
-        consoleMessages
-      );
-    });
+    //   cy.validateGtmEvent(
+    //     "drivers_tab_click",
+    //     gtmExpectedEvents.drivers_tab_click,
+    //     consoleMessages
+    //   );
+    // });
 
-    it("tests open_help_menu_click event payload", () => {
-      cy.contains("Help", { timeout: 10000 }).click();
+    // it("tests open_help_menu_click event payload", () => {
+    //   cy.contains("Help", { timeout: 10000 }).click();
 
-      cy.validateGtmEvent(
-        "open_help_menu_click",
-        gtmExpectedEvents.open_help_menu_click,
-        consoleMessages
-      );
-    });
+    //   cy.validateGtmEvent(
+    //     "open_help_menu_click",
+    //     gtmExpectedEvents.open_help_menu_click,
+    //     consoleMessages
+    //   );
+    // });
 
     it("tests help_menu_item_click event payload", () => {
       cy.contains("Help", { timeout: 10000 }).click();
@@ -428,49 +428,49 @@ describe("analytics Test Suite", () => {
       );
     });
 
-    it("tests drawer_close_click event payload", () => {
-      cy.contains("Help", { timeout: 10000 }).click();
-      cy.contains("Help Center", { timeout: 10000 }).click();
-      cy.get("button.bg-gray-150.w-7.h-7.rounded-full")
-        .should("be.visible")
-        .click({ force: true });
+    // it("tests drawer_close_click event payload", () => {
+    //   cy.contains("Help", { timeout: 10000 }).click();
+    //   cy.contains("Help Center", { timeout: 10000 }).click();
+    //   cy.get("button.bg-gray-150.w-7.h-7.rounded-full")
+    //     .should("be.visible")
+    //     .click({ force: true });
 
-      cy.validateGtmEvent(
-        "drawer_close_click",
-        gtmExpectedEvents.drawer_close_click,
-        consoleMessages
-      );
-    });
+    //   cy.validateGtmEvent(
+    //     "drawer_close_click",
+    //     gtmExpectedEvents.drawer_close_click,
+    //     consoleMessages
+    //   );
+    // });
 
-    it("tests internal_link_click event payload", () => {
-      cy.contains("Help", { timeout: 10000 }).click();
-      cy.contains("Help Center", { timeout: 10000 }).click();
-      cy.contains("Comprehensive Example Article", { timeout: 10000 }).click();
-      cy.contains("internal links to other articles", {
-        timeout: 10000,
-      }).click();
+    // it("tests internal_link_click event payload", () => {
+    //   cy.contains("Help", { timeout: 10000 }).click();
+    //   cy.contains("Help Center", { timeout: 10000 }).click();
+    //   cy.contains("Comprehensive Example Article", { timeout: 10000 }).click();
+    //   cy.contains("internal links to other articles", {
+    //     timeout: 10000,
+    //   }).click();
 
-      cy.validateGtmEvent(
-        "internal_link_click",
-        gtmExpectedEvents.internal_link_click,
-        consoleMessages
-      );
-    });
+    //   cy.validateGtmEvent(
+    //     "internal_link_click",
+    //     gtmExpectedEvents.internal_link_click,
+    //     consoleMessages
+    //   );
+    // });
 
-    it("tests external_link_click event payload", () => {
-      cy.contains("Help", { timeout: 10000 }).click();
-      cy.contains("Help Center", { timeout: 10000 }).click();
-      cy.contains("Comprehensive Example Article", { timeout: 10000 }).click();
-      cy.contains("contextual links to external sites", {
-        timeout: 10000,
-      }).click();
+    // it("tests external_link_click event payload", () => {
+    //   cy.contains("Help", { timeout: 10000 }).click();
+    //   cy.contains("Help Center", { timeout: 10000 }).click();
+    //   cy.contains("Comprehensive Example Article", { timeout: 10000 }).click();
+    //   cy.contains("contextual links to external sites", {
+    //     timeout: 10000,
+    //   }).click();
 
-      cy.validateGtmEvent(
-        "external_link_click",
-        gtmExpectedEvents.external_link_click,
-        consoleMessages
-      );
-    });
+    //   cy.validateGtmEvent(
+    //     "external_link_click",
+    //     gtmExpectedEvents.external_link_click,
+    //     consoleMessages
+    //   );
+    // });
 
     it("tests help_menu_item_click_lexicon event payload", () => {
       cy.contains("Help", { timeout: 10000 }).click();
