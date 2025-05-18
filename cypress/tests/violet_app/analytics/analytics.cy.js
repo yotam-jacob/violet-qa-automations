931c51ff85ca03001c489fa0ea678e148fe10ddc
yotamjacob <yotamjacob@gmail.com>
2025-05-18 16:14:21 +0300

    feat: add tests for three_dots_default and three_dots_public event payloads in GTM events validation

--- cypress/tests/violet_app/analytics/analytics.cy.js
+++ cypress/tests/violet_app/analytics/analytics.cy.js
@@ -36,6 +36,85 @@ describe("analytics Test Suite", () => {
   });
 
   describe("GTM Events Validation", () => {
+    it("tests three_dots_default event payload", () => {
+      //Create new view and set as default
+      cy.createView(AUTOMATION_VIEW_NAME, { isDefault: true });
+
+      cy.contains(AUTOMATION_VIEW_NAME, { timeout: 40000 }).should(
+        "be.visible"
+      );
+
+      //Verify that the new view is set as default
+      cy.reload();
+      cy.get("#__next", { timeout: 45000 }).should("exist");
+
+      cy.contains(AUTOMATION_VIEW_NAME, { timeout: 40000 }).click();
+      cy.wait(3000);
+      //Hover over the "Default" view and click the 3-dots menu
+      cy.contains("Default").realHover();
+      cy.wait(3000);
+      cy.clickVisibleThreeDots();
+      cy.wait(3000);
+
+      //uncheck the default view
+      cy.get("#isDefault", { timeout: 45000 }).click();
+
+      cy.reload({ timeout: 45000 });
+      cy.get("#__next", { timeout: 45000 }).should("exist");
+
+      cy.contains(AUTOMATION_VIEW_NAME, { timeout: 10000 }).click();
+      cy.contains("Default").realHover();
+      cy.clickVisibleThreeDots();
+      //Delete the view
+      cy.clickOnDeleteViewAndVerify();
+
+      cy.validateGtmEvent(
+        "3-dots view menu click",
+        gtmExpectedEvents.three_dots_default,
+        consoleMessages
+      );
+    });
+
+    it("tests three_dots_public event payload", () => {
+      cy.wait(3000);
+
+      //Create new public view
+      cy.createView(AUTOMATION_VIEW_NAME, { isPublic: true });
+
+      cy.reload();
+      cy.get("#__next", { timeout: 45000 }).should("exist");
+
+      // Verify it is set as public
+
+      cy.contains("Views", { timeout: 40000 }).click();
+
+      //Hover over the AUTOMATION_VIEW_NAME view and click the 3-dots menu
+      cy.contains("div.w-full", AUTOMATION_VIEW_NAME)
+        .find("div.group\\/item.relative")
+        .realHover();
+
+      cy.contains("You are sharing this view as a Team view").should(
+        "be.visible"
+      );
+
+      //Change the view to non public
+      cy.clickVisibleThreeDots();
+
+      cy.get("#isPublic").click();
+      cy.contains("You are sharing this view as a Team view").should(
+        "not.exist"
+      );
+
+      // Delete the view
+      cy.clickOnDeleteViewAndVerify();
+
+      cy.validateGtmEvent(
+        "3-dots view menu click",
+        gtmExpectedEvents.three_dots_public,
+        consoleMessages
+      );
+    });
+
     it("tests report_click event payload", () => {
       cy.visit(
         "https://dev.violetgrowth.com/partners/qa/reports/kpi-trendlines"
@@ -400,85 +479,6 @@ describe("analytics Test Suite", () => {
 
       cy.clickVisibleThreeDots();
       cy.clickOnDeleteViewAndVerify();
-    });
-
-    it("tests three_dots_default event payload", () => {
-      //Create new view and set as default
-      cy.createView(AUTOMATION_VIEW_NAME, { isDefault: true });
-
-      cy.contains(AUTOMATION_VIEW_NAME, { timeout: 40000 }).should(
-        "be.visible"
-      );
-
-      //Verify that the new view is set as default
-      cy.reload();
-      cy.get("#__next", { timeout: 45000 }).should("exist");
-
-      cy.contains(AUTOMATION_VIEW_NAME, { timeout: 40000 }).click();
-      cy.wait(3000);
-      //Hover over the "Default" view and click the 3-dots menu
-      cy.contains("Default").realHover();
-      cy.wait(3000);
-      cy.clickVisibleThreeDots();
-      cy.wait(3000);
-
-      //uncheck the default view
-      cy.get("#isDefault", { timeout: 45000 }).click();
-
-      cy.reload({ timeout: 45000 });
-      cy.get("#__next", { timeout: 45000 }).should("exist");
-
-      cy.contains(AUTOMATION_VIEW_NAME, { timeout: 10000 }).click();
-      cy.contains("Default").realHover();
-      cy.clickVisibleThreeDots();
-      //Delete the view
-      cy.clickOnDeleteViewAndVerify();
-
-      cy.validateGtmEvent(
-        "3-dots view menu click",
-        gtmExpectedEvents.three_dots_default,
-        consoleMessages
-      );
-    });
-
-    it("tests three_dots_public event payload", () => {
-      cy.wait(3000);
-
-      //Create new public view
-      cy.createView(AUTOMATION_VIEW_NAME, { isPublic: true });
-
-      cy.reload();
-      cy.get("#__next", { timeout: 45000 }).should("exist");
-
-      // Verify it is set as public
-
-      cy.contains("Views", { timeout: 40000 }).click();
-
-      //Hover over the AUTOMATION_VIEW_NAME view and click the 3-dots menu
-      cy.contains("div.w-full", AUTOMATION_VIEW_NAME)
-        .find("div.group\\/item.relative")
-        .realHover();
-
-      cy.contains("You are sharing this view as a Team view").should(
-        "be.visible"
-      );
-
-      //Change the view to non public
-      cy.clickVisibleThreeDots();
-
-      cy.get("#isPublic").click();
-      cy.contains("You are sharing this view as a Team view").should(
-        "not.exist"
-      );
-
-      // Delete the view
-      cy.clickOnDeleteViewAndVerify();
-
-      cy.validateGtmEvent(
-        "3-dots view menu click",
-        gtmExpectedEvents.three_dots_public,
-        consoleMessages
-      );
     });
   });
 });
