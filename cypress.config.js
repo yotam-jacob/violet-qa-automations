d88f8ff4c0ef7f19ab5a1d6e1ce0233162100cb1
yotamjacob <yotamjacob@gmail.com>
2025-07-07 12:52:35 +0300

    refactor: update specPattern quotes for consistency and uncomment tests for future implementation

--- cypress.config.js
+++ cypress.config.js
@@ -11,6 +11,7 @@ module.exports = defineConfig({
   video: true,
   screenshotsFolder: "cypress/screenshots",
   videosFolder: "cypress/videos",
+  videoCompression: false,
 
   e2e: {
     setupNodeEvents(on, config) {
@@ -22,7 +23,7 @@ module.exports = defineConfig({
       });
     },
     baseUrl: "https://dev.violetgrowth.com",
-    specPattern: 'cypress/tests/violet_app/**/*.js',
+    specPattern: "cypress/tests/violet_app/**/*.js",
     viewportWidth: 1920,
     screenshotOnRunFailure: false,
     viewportHeight: 1080,
