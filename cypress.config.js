67d846748bbee5757529e500adf7c17b2a0058f3
yotamjacob <yotamjacob@gmail.com>
2025-07-13 10:57:34 +0300

    refactor: move pageLoadTimeout configuration into e2e block for better organization

--- cypress.config.js
+++ cypress.config.js
@@ -8,13 +8,14 @@ module.exports = defineConfig({
     },
   },
 
-  pageLoadTimeout: 240000, // increase to 4 minutes
   video: true,
   screenshotsFolder: "cypress/screenshots",
   videosFolder: "cypress/videos",
   videoCompression: false,
 
   e2e: {
+    pageLoadTimeout: 240000, // increase to 4 minutes
+
     setupNodeEvents(on, config) {
       on("task", {
         logToTerminal(msg) {
@@ -22,13 +23,22 @@ module.exports = defineConfig({
           return null;
         },
       });
+
+      on("before:browser:launch", (browser = {}, launchOptions) => {
+        if (browser.name === "chrome" || browser.name === "chromium") {
+          launchOptions.args.push("--no-sandbox");
+          launchOptions.args.push("--disable-gpu");
+          launchOptions.args.push("--disable-dev-shm-usage");
+        }
+        return launchOptions;
+      });
     },
+
     baseUrl: "https://dev.violetgrowth.com",
     specPattern: "cypress/tests/violet_app/**/*.js",
     viewportWidth: 1920,
-    screenshotOnRunFailure: false,
     viewportHeight: 1080,
+    screenshotOnRunFailure: false,
     videoCompression: false,
-    pageLoadTimeout: 240000, // increase to 4 minutes
   },
 });
