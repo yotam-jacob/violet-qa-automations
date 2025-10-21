0ea4f586f6f4ffc474d725f52e5b9843643d7606
yotamjacob <yotamjacob@gmail.com>
2025-10-21 21:45:29 +0700

    refactor: update timeout for loginToVioletStg command from 15000ms to 25000ms

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,7 +24,7 @@ Cypress.Commands.add("loginToVioletStg", () => {
   });
 
   cy.visit(url, {
-    timeout: 15000,
+    timeout: 25000,
   });
 
   cy.contains("Sign in with email", { timeout: 60000 }).click();
