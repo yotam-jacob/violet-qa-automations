543783fa2516777467411b9eb823c832938c5a24
yotamjacob <yotamjacob@gmail.com>
2025-11-11 08:49:20 +0700

    increase port size

--- cypress.config.js
+++ cypress.config.js
@@ -4,5 +4,7 @@ module.exports = defineConfig({
   e2e: {
     specPattern: "cypress/tests/**/*.cy.js",
     supportFile: "cypress/support/e2e.js",
+    viewportWidth: 1920,
+    viewportHeight: 1080,
   },
 });
