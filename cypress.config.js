cd8155bf62f4eb95ea67e1fe3358d56b8b721aa4
yotamjacob <yotamjacob@gmail.com>
2025-11-11 09:31:15 +0700

    try older stuff

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,32 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+  },
+
+  pageLoadTimeout: 120000,
+  video: true,
+  screenshotsFolder: "cypress/screenshots",
+  videosFolder: "cypress/videos",
+  videoCompression: false,
+
   e2e: {
-    specPattern: "cypress/tests/**/*.cy.js",
-    supportFile: "cypress/support/e2e.js",
+    setupNodeEvents(on, config) {
+      on("task", {
+        logToTerminal(msg) {
+          console.log(msg);
+          return null;
+        },
+      });
+    },
+    baseUrl: "https://dev.violetgrowth.com",
+    specPattern: "cypress/tests/violet_app/**/*.js",
     viewportWidth: 1920,
+    screenshotOnRunFailure: false,
     viewportHeight: 1080,
-    video: true,
-    videosFolder: "cypress/videos",
   },
 });
--- cypress/tests/codex/sites_access_demo.cy.js
+++ cypress/tests/codex/sites_access_demo.cy.js
@@ -1,9 +1,4 @@
-describe("Codex Public Availability", () => {
-  it("loads google.com successfully", () => {
-    cy.visit("https://www.google.com");
-    cy.title({ timeout: 30000 }).should("include", "Google");
-  });
-
+describe("Demo Public Availability", () => {
   it("shows Sign in with email on Dev", () => {
     cy.visit("https://dev.violetgrowth.com", { failOnStatusCode: false });
     cy.contains("Sign in with email", { timeout: 30000 }).should("be.visible");
