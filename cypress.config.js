77fd2397148808b34077388ccd80098e3ccf7b90
yotamjacob <yotamjacob@gmail.com>
2025-11-11 11:15:02 +0700

    add ip tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  video: true,
+  videosFolder: "cypress/videos",
   e2e: {
     specPattern: "cypress/tests/**/*.cy.js",
     supportFile: "cypress/support/e2e.js",
