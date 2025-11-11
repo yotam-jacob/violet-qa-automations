b5608289017bb60d9bd60c2193c2a90853332d83
yotamjacob <yotamjacob@gmail.com>
2025-11-11 08:35:54 +0700

    fix basic logins

--- cypress/tests/codex/codex_public_sites.cy.js
+++ cypress/tests/codex/codex_public_sites.cy.js
@@ -1,7 +1,7 @@
 describe("Codex Public Availability", () => {
   it("loads google.com successfully", () => {
     cy.visit("https://www.google.com");
-    cy.get("input[name='q']", { timeout: 30000 }).should("be.visible");
+    cy.title({ timeout: 30000 }).should("include", "Google");
   });
 
   it("shows Sign in with email on Dev", () => {
@@ -10,7 +10,7 @@ describe("Codex Public Availability", () => {
   });
 
   it("shows Sign in with email on Staging", () => {
-    cy.visit("https://stg.violetgrowth.com/", { failOnStatusCode: false });
+    cy.visit("https://staging.violetgrowth.com/", { failOnStatusCode: false });
     cy.contains("Sign in with email", { timeout: 30000 }).should("be.visible");
   });
 
